92ab58555d80cf4fd0af8193d4a7eb7911c116b9
Sara I <phbyjulian@gmail.com>
2024-10-20 11:10:28 -0500

    Completada vista inicial con animaciones

--- main.js
+++ main.js
@@ -13,3 +13,40 @@ navLinks.addEventListener("click",(e) => {
     navLinks.classList.remove("open");
     menuBtnIcon.setAttribute("class", "ri-menu-4-fill");
 });
+
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000
+}
+
+ScrollReveal().reveal(".header__image img", {
+    ...scrollRevealOption,
+    origin: "right",
+})
+
+ScrollReveal().reveal(".header__content h1", {
+    ...scrollRevealOption,
+    delay: 500
+})
+
+ScrollReveal().reveal(".header__content p", {
+    ...scrollRevealOption,
+    delay: 500
+})
+
+ScrollReveal().reveal(".header__content form", {
+    ...scrollRevealOption,
+    delay: 500
+})
+
+ScrollReveal().reveal(".header__content .bar", {
+    ...scrollRevealOption,
+    delay: 500
+})
+
+ScrollReveal().reveal(".header__image__card", {
+    duration: 1000,
+    interval: 500,
+    delay: 1000
+})
