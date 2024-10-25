21974432fd506dba08bdc1fb16959aa35c2d0888
Sara I <phbyjulian@gmail.com>
2024-10-25 16:18:29 -0500

    Agregar about

--- main.js
+++ main.js
@@ -50,3 +50,51 @@ ScrollReveal().reveal(".header__image__card", {
     interval: 500,
     delay: 1000
 })
+
+// About animations
+ScrollReveal().reveal(".about__izq h1", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".about__izq .container__btn", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".about__der h4", {
+  ...scrollRevealOption,
+  delay: 2000,
+});
+ScrollReveal().reveal(".about__der h2", {
+  ...scrollRevealOption,
+  delay: 2500,
+});
+ScrollReveal().reveal(".about__der h3", {
+  ...scrollRevealOption,
+  delay: 3000,
+});
+ScrollReveal().reveal(".about__der p", {
+  ...scrollRevealOption,
+  delay: 3500,
+});
+
+ScrollReveal().reveal(".about__der .tent-1", {
+  duration: 1000,
+  delay: 4000,
+});
+ScrollReveal().reveal(".about__der .tent-2", {
+  duration: 1000,
+  delay: 4500,
+});
+
+ScrollReveal().reveal(".location", {
+  ...scrollRevealOption,
+  origin: "left",
+  delay: 5000,
+});
+
+ScrollReveal().reveal(".socials span", {
+  ...scrollRevealOption,
+  origin: "top",
+  delay: 5500,
+  interval: 500,
+});
